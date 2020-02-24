8b553d0eeec52b869021526450a00d87357fd6f0
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-24 17:33:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)
   new = []
-  array.collect { |element| new << }
+  array.collect { |element| new <<  }
 end
 
 def long_planeteer_calls# code an argument here
