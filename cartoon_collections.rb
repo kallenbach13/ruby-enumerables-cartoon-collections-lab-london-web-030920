9e8417d3938b4bd30d0f2ad0aae0ce96fc2f1ef1
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-24 17:34:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)
   new = []
-  array.collect { |element| new << "#{element.capitaliz}!" }
+  array.collect { |element| new << "#{element.capitalize}!" }
   new
 end
 
